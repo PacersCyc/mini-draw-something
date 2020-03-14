07425c8e82f3871bb4d0ced334cb612a403f9eea
PacersCyc <452720855@qq.com>
2020-03-14 18:07:28 +0800

    github pages change

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, '../dist'),
     filename: 'js/[name].[hash].js',
-    publicPath: '/'
+    publicPath: './'
   },
   module: {
     rules: [
